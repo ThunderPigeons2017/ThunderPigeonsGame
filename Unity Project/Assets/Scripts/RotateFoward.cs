80cd99a7b1ba187aae146a8822829e9808195855
GeneDC <genecollis12@gmail.com>
2017-09-13 12:28:07 +1000

    Changed RotateFoward script to face towards velocity, still needs work Added Assets/3rdPartyAssets/XInput

--- Unity Project/Assets/Scripts/RotateFoward.cs
+++ Unity Project/Assets/Scripts/RotateFoward.cs
@@ -8,12 +8,31 @@ public class RotateFoward : MonoBehaviour
 	[SerializeField]
 	float maxRotation;
 
-	void FixedUpdate ()
+    [SerializeField]
+    GameObject playerBall;
+
+    Rigidbody rb;
+
+    void Awake()
+    {
+        rb = playerBall.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate ()
 	{
 		float moveHorizontal = Input.GetAxis("Horizontal");
 		float moveVertical = Input.GetAxis("Vertical");
 
-		Quaternion rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(moveVertical * maxRotation, 0.0f, -moveHorizontal * maxRotation), Time.deltaTime * 10.0f);
+        Vector3 velocity = rb.velocity;
+        velocity.y = 0.0f;
+
+        //Quaternion lookRotation = Quaternion.LookRotation(velocity);
+        //transform.rotation = lookRotation;
+
+        float yRotation = Vector3.SignedAngle(Vector3.forward, velocity.normalized, Vector3.up);
+
+        Quaternion rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(transform.rotation.eulerAngles.x, yRotation, transform.rotation.eulerAngles.z), Time.deltaTime * 100.0f);
+        rotation = Quaternion.Slerp(rotation, Quaternion.Euler(moveVertical * maxRotation, rotation.eulerAngles.y, -moveHorizontal * maxRotation), Time.deltaTime * 10.0f);
 
 		transform.rotation = rotation;
 	}
