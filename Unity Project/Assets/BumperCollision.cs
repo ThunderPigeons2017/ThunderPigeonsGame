100e06a933db59867a631dfb05aeeae45564ae35
GeneDC <genecollis12@gmail.com>
2017-09-27 12:58:18 +1000

    Added BumperCollision.cs still early dev

---
+++ Unity Project/Assets/BumperCollision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperCollision : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject playerBall;
+
+    Rigidbody playerBallrb;
+	void Start ()
+    {
+        playerBallrb = playerBall.GetComponent<Rigidbody>();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PlayerBody")
+        {
+            Rigidbody otherRigid = other.GetComponent<Rigidbody>();
+
+            Vector3 vecBetween = playerBall.transform.position - other.transform.position;
+            vecBetween.y = 0.0f;
+            if (Mathf.Abs(Vector3.Angle(playerBallrb.velocity, vecBetween)) > 10f)
+            {
+                otherRigid.AddForce(playerBallrb.velocity, ForceMode.Impulse);
+                //playerBallrb.AddForce(-playerBallrb.velocity, ForceMode.Impulse);
+            }
+        }
+    }
+}
