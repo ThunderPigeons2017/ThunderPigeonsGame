0036e2211914153f1b57beed74f5f3c8cb41f950
GeneDC <genecollis12@gmail.com>
2017-10-02 10:19:22 +1100

    Added ZoneControl.cs and GameManager.cs

---
+++ Unity Project/Assets/Scripts/ZoneControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneControl : MonoBehaviour
+{
+    public List<GameObject> playersInZone;
+
+	void Start ()
+    {
+        playersInZone = new List<GameObject>();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Add the player to the list
+        if (other.tag == "Player")
+        {
+            playersInZone.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Remove the players from the list
+        if (other.tag == "Player")
+        {
+            playersInZone.Remove(other.gameObject);
+        }
+    }
+}
