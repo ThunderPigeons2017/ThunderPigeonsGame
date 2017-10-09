4ab1b9873b0d246409553c4ba4279bc82af688d7
AnonUser676 <rouie.ortega@live.com>
2017-10-09 15:29:24 +1100

    Updated Camera Control to Zoom In  - RJ - XD

--- Unity Project/Assets/Scripts/CameraControl.cs
+++ Unity Project/Assets/Scripts/CameraControl.cs
@@ -8,32 +8,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraControl : MonoBehaviour {
+public class CameraControl : MonoBehaviour
+{
 
     public float m_DampTime = 0.2f;                             //approximate time for the camera to move to new location
     public float m_ScreenEdgeBufer = 4f;                        //added to sides to ensure players don't go off camera
     public float m_MinSize = 6.5f;                              //stops camera from zooming in
+    public float ceiling = 5f;
+    public float buffer = 2f;
 
     private PlayerController[] m_Targets = new PlayerController[4];           //array of gaming objects that would be the targets for camera to adjust to
     private Camera m_Camera;                                    //reference to the camera attached as child
     private float m_ZoomSpeed;                                  //damps zooming, slows it down to make it less jarring
     private Vector3 m_MoveVelocity;                             //damps camera movement and panning to avoid jarring camera movements
     private Vector3 m_DesiredPosition;                          //position that camera is trying to reach
+    
 
     [SerializeField]
     private GameObject m_gmObject;
     private GameManager m_gm;
 
-	// Use this for initialization
-	private void Awake ()
+    // Use this for initialization
+    private void Awake()
     {
         m_Camera = GetComponentInChildren<Camera>();	        //references camera and gets values
 
         m_gm = m_gmObject.GetComponent<GameManager>();
-	}
+    }
 
     //this updates every runtime
-    private void FixedUpdate()                                  
+    private void FixedUpdate()
     {
         for (int i = 0; i < 4; i++)
         {
@@ -41,21 +45,30 @@ public class CameraControl : MonoBehaviour {
             {
                 m_Targets[i] = m_gm.players[i].GetComponentInChildren<PlayerController>();
             }
-            //else
-            //{
-            //    m_Targets[i] = null;
-            //}
+            else
+            {
+                m_Targets[i] = null;
+            }
         }
 
         Move();                                                //calls move function to move camera
-        //Zoom();                                                //calls Zoom function to Zoom Camera
+       
     }
 
     private void Move()
     {
         FindAveragePosition();                                //Function that tries to find all playerobjects target position
 
-        transform.position = Vector3.SmoothDamp(transform.position, m_DesiredPosition, ref m_MoveVelocity, m_DampTime); //
+        float distance = FindDistance();
+
+        if (distance <= ceiling)
+        {
+            distance = ceiling;
+        }
+
+        m_DesiredPosition += - m_Camera.transform.forward * distance * buffer;
+
+        m_Camera.transform.position = Vector3.SmoothDamp(m_Camera.transform.position, m_DesiredPosition, ref m_MoveVelocity, m_DampTime);
     }
 
     private void FindAveragePosition()
@@ -75,55 +88,49 @@ public class CameraControl : MonoBehaviour {
         if (numTargets > 0)
             averagePos /= numTargets;
 
-        averagePos.y = transform.position.y;
-
         m_DesiredPosition = averagePos;
     }
 
-    private void Zoom()
+    public float FindDistance()
     {
-        float requiredSize = FindRequiredSize();
-        m_Camera.orthographicSize = Mathf.SmoothDamp(m_Camera.orthographicSize, requiredSize, ref m_ZoomSpeed, m_DampTime);
+        float distance = 0f;
 
-        Vector3 targetPosition = Vector3.zero;
+        Vector3 averagePos = new Vector3();
+        int numTargets = 0;
 
-        // Smoothly moves the camera to the target position
-        m_Camera.transform.position = Vector3.Lerp(m_Camera.transform.position, targetPosition, 10f * Time.fixedDeltaTime);
-    }
-
-    private float FindRequiredSize()
-    {
-        Vector3 desiredLocalPos = transform.InverseTransformPoint(m_DesiredPosition);
-
-        float size = 0f;
 
         for (int i = 0; i < m_Targets.Length; i++)
         {
             if (m_Targets[i] == null || !m_Targets[i].isAlive)
                 continue;
 
-            Vector3 targetLocalPos = transform.InverseTransformPoint(m_Targets[i].gameObject.transform.position);
-
-            Vector3 desiredPosToTarget = targetLocalPos - desiredLocalPos;
-
-            size = Mathf.Max(size, Mathf.Abs(desiredPosToTarget.y));
-            size = Mathf.Max(size, Mathf.Abs(desiredPosToTarget.x) / m_Camera.aspect);
+            averagePos += m_Targets[i].gameObject.transform.position;
+            numTargets++;
         }
 
-        size += m_ScreenEdgeBufer;
+        if (numTargets > 0)
+            averagePos /= numTargets;
 
-        size = Mathf.Max(size, m_MinSize);
+        float targetDistanceToAverage = 0f;
+        float temporaryContainer = 0f;
 
-        return size;
+        for (int i = 0; i < m_Targets.Length; i++)
+        {
+            if (m_Targets[i] == null || !m_Targets[i].isAlive)
+                continue;
+
+            temporaryContainer = Vector3.Distance(m_Targets[i].gameObject.transform.position, averagePos);
+            targetDistanceToAverage = Mathf.Max(temporaryContainer, targetDistanceToAverage);
+        }
+
+        return distance = targetDistanceToAverage;
     }
-
+    
     public void SetStartPositionAndSize()
     {
         FindAveragePosition();
 
         transform.position = m_DesiredPosition;
-
-        m_Camera.orthographicSize = FindRequiredSize();
     }
 
 }
