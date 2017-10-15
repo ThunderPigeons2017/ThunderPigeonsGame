f969a28da38409b083fbee3c2667ffa3986bd234
GeneDC <genecollis12@gmail.com>
2017-10-16 10:15:03 +1100

    Added Main Menu.unity Added MenuCameraControl.cs

---
+++ Unity Project/Assets/Scripts/Camera/MenuCameraControl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCameraControl : MonoBehaviour
+{
+    [SerializeField]
+    Transform startTransfrom;
+    [SerializeField]
+    Transform characterSelectTransform;
+    [SerializeField]
+    float speed;
+
+    Transform targetTransform;
+
+    Camera camera;
+
+    private float startTime;
+    private float journeyLength;
+
+    void Awake()
+    {
+        camera = GetComponent<Camera>();
+        targetTransform = startTransfrom;
+	}
+	
+	void LateUpdate()
+    {
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = 0;
+        if (journeyLength != 0) // Do not divide by zero
+        {
+            fracJourney = distCovered / journeyLength;
+        }
+
+        // Lerp position
+        camera.transform.position = Vector3.Lerp(camera.transform.position, targetTransform.position, fracJourney);
+        // Lerp rotation
+        camera.transform.rotation = Quaternion.Lerp(camera.transform.rotation, targetTransform.rotation, fracJourney);
+    }
+
+    public void MoveToStart()
+    {
+        targetTransform = startTransfrom;
+
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(camera.transform.position, targetTransform.position);
+    }
+
+    public void MoveToCharacterSelection()
+    {
+        targetTransform = characterSelectTransform;
+
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(camera.transform.position, targetTransform.position);
+    }
+}
