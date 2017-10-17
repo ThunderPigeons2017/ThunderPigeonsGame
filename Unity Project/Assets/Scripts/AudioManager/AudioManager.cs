1107596c0f7849291d6a04348b9d1c60dda77d83
AnonUser676 <rouie.ortega@live.com>
2017-10-17 12:09:44 +1100

    Audio Manager pseudo for use in game

---
+++ Unity Project/Assets/Scripts/AudioManager/AudioManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public Sound[] sounds;
+
+	// Use this for initialization
+	void Awake ()
+    {
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.loop = s.Loop;
+        }		
+	}
+
+    void Start()
+    {
+        Play("Music");
+    }
+
+    // Update is called once per frame
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+            return;
+
+        s.source.Play();
+    }
+}
