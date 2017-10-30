69495aeef9005eec856dea6de1e06afec9ca5652
AnonUser676 <rouie.ortega@live.com>
2017-10-30 16:34:52 +1100

    Pause Menu added in RJ Sandbox, needs implementation to Beta Scene

--- Unity Project/Assets/Scenes/Sandbox/Programmer/PauseGame.cs
+++ Unity Project/Assets/Scenes/Sandbox/Programmer/PauseGame.cs
@@ -7,7 +7,9 @@ using XboxCtrlrInput;
 public class PauseGame : MonoBehaviour {
 
     public Transform canvas;
-    public Transform canvas2;
+   // public Transform canvas2;
+    public Transform optionsPanel;
+    public Transform pauseMenu;
     public XboxController controller;
 
     // Use this for initialization
@@ -28,13 +30,17 @@ public class PauseGame : MonoBehaviour {
         if (canvas.gameObject.activeInHierarchy == false)
         {
             canvas.gameObject.SetActive(true);
-            canvas2.gameObject.SetActive(false);
+            //canvas2.gameObject.SetActive(false);
+            optionsPanel.gameObject.SetActive(false);
+            pauseMenu.gameObject.SetActive(true);
             Time.timeScale = 0;
         }
         else
         {
             canvas.gameObject.SetActive(false);
-            canvas2.gameObject.SetActive(true);
+           // canvas2.gameObject.SetActive(true);
+            optionsPanel.gameObject.SetActive(false);
+            pauseMenu.gameObject.SetActive(true);
             Time.timeScale = 1;
         }
     }
