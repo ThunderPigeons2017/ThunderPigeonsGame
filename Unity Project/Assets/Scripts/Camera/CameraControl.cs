b59580bf01b4567758f3638d0e9f5e4dea518d6f
AnonUser676 <rouie.ortega@live.com>
2017-10-31 16:58:27 +1100

    Camera now does not follow Zone Camera only follows players indicating if zone is on left or right if zone is out of view Camera goes to defaultCamPos if there are no active players in Beta

--- Unity Project/Assets/Scripts/Camera/CameraControl.cs
+++ Unity Project/Assets/Scripts/Camera/CameraControl.cs
@@ -12,91 +12,144 @@ public class CameraControl : MonoBehaviour
 {
 
     public float m_DampTime = 0.2f;                                             //approximate time for the camera to move to new location
-    public float minimumDistance = 5f;
-    public float maximumDistance = 75f;
-    public float distanceScale = 1.5f;
-    public bool followZone = true;
-
+    public float minimumDistance = 5f;                                          //minimum distance for zoom
+    public float maximumDistance = 75f;                                         //maximum distance for zoom
+    public float distanceScale = 1.5f;                                          //distance zoom scale
+    public bool followZone = true;                                              //checks if zone is to be followed
+    
     private PlayerController[] m_Targets = new PlayerController[4];             //array of gaming objects that would be the targets for camera to adjust to
     private Camera m_Camera;                                                    //reference to the camera attached as child
     private float m_ZoomSpeed;                                                  //damps zooming, slows it down to make it less jarring
     private Vector3 m_MoveVelocity;                                             //damps camera movement and panning to avoid jarring camera movements
     private Vector3 m_DesiredPosition;                                          //position that camera is trying to reach
-
-
+    private Vector3 screenPoint;                                                //variable that holds frustrum position
+   
     [SerializeField]
-    private GameObject m_gmObject;
-    private GameObject m_Zone;
-    private GameManager m_gm;
+    private GameObject m_gmObject;                                              //Instance for game Object
+    private GameObject m_Zone;                                                  //Instance for game zone
+    private GameManager m_gm;                                                   //Instance for Game Manager
+    public GameObject indicatorLeft;                                            //Game Object left arrow
+    public GameObject indicatorRight;                                           //Game Object right arrow
+    public GameObject defaultCamPos;                                            //Game Object default Camera Position
 
-    // Use this for initialization
+    /// <summary>
+    /// Awake gets initial values on Initialization
+    /// </summary>
     private void Awake()
     {
         m_Camera = GetComponent<Camera>();	                                    //references camera and gets values
 
-        m_Zone = GameObject.FindGameObjectWithTag("Zone");
+        m_Zone = GameObject.FindGameObjectWithTag("Zone");                      //gets zone Instance
 
-        m_gm = m_gmObject.GetComponent<GameManager>();
+        m_gm = m_gmObject.GetComponent<GameManager>();                          //gets values for game manager objects
     }
 
-    //this updates every runtime
+    /// <summary>
+    /// Runs on every Update
+    /// </summary>
     private void FixedUpdate()
     {
         for (int i = 0; i < 4; i++)
         {
             if (m_gm.players[i] != null)
             {
-                m_Targets[i] = m_gm.players[i].GetComponentInChildren<PlayerController>();
+                m_Targets[i] = m_gm.players[i].GetComponentInChildren<PlayerController>();                    //checks number of active players
             }
             else
             {
                 m_Targets[i] = null;
             }
         }
-
+        
+        if (!CanFindZone())                                                                                  //calls function to find if zone is in view of camera
+        {
+            screenPoint = m_Camera.WorldToViewportPoint(m_Zone.transform.position);                           //gets location oz zone in relation to camera frustrum
+            if (screenPoint.x < 0)                                                                            //if zone is out of view and on the left
+            {
+                indicatorLeft.SetActive(true);
+                indicatorRight.SetActive(false);
+            }
+            else                                                                                              //if zone is out of view and on right
+            {
+                indicatorLeft.SetActive(false);
+                indicatorRight.SetActive(true);
+            }
+        }
+        else                                                                                                  //if zone is in view
+        {
+            indicatorLeft.SetActive(false);
+            indicatorRight.SetActive(false);
+        }
         Move();                                                                 //calls move function to move camera
 
     }
 
-    private void Move()
+    /// <summary>
+    /// Function that checks if zone is in view of camera fustrum
+    /// </summary>
+    /// <returns>returns true if in view or false if not</returns>
+    private bool CanFindZone()
     {
-        m_DesiredPosition = FindAveragePosition();                                //Function that tries to find all playerobjects target position
-
-        float distance = FindDistance();
-
-        if (distance <= minimumDistance)
-        {
-            distance = minimumDistance;
-        }
-        else if (distance >= maximumDistance)
-        {
-            distance = maximumDistance;
-        }
-
-
-        m_DesiredPosition += -m_Camera.transform.forward * distance * distanceScale;
-
-        m_Camera.transform.position = Vector3.SmoothDamp(m_Camera.transform.position, m_DesiredPosition, ref m_MoveVelocity, m_DampTime);
+        screenPoint = m_Camera.WorldToViewportPoint(m_Zone.transform.position);                                        //gets location oz zone in relation to camera frustrum
+        if (screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1)     //checks if location of zone is within frustrum
+            return true;
+        else
+            return false;
     }
 
+    /// <summary>
+    /// Moves Camera to desired position
+    /// </summary>
+    private void Move()
+    {
+        m_DesiredPosition = FindAveragePosition();                                //sets desired position to be result of function FindAveragePosition
+
+        float distance = FindDistance();                                          //Function called to find Distance between players/zone
+
+        if (distance <= minimumDistance)                                          //Checks if distance is under minimum distance
+        {
+            distance = minimumDistance;                                           //sets distance to minimum distance if distance is under minumum
+        }
+        else if (distance >= maximumDistance)                                     //Checks if distance is over maximum distance
+        {
+            distance = maximumDistance;                                           //sets distance to maximum distance if distance is over maximum
+        }
+        
+        m_DesiredPosition += -m_Camera.transform.forward * distance * distanceScale; //sets zoom distance based on distance value and distance scale
+
+        m_Camera.transform.position = Vector3.SmoothDamp(m_Camera.transform.position, m_DesiredPosition, ref m_MoveVelocity, m_DampTime); //transforms camera position based on desired position and movement velocity based on MoveVelocity and DampTime
+    }
+
+    /// <summary>
+    /// Finds Average position between players and checks if there are any players left on the boat
+    /// </summary>
+    /// <returns> average position of players or zoom out position </returns>
     private Vector3 FindAveragePosition()
     {
-        Vector3 averagePos = new Vector3();
-        int numTargets = 0;
+        Vector3 averagePos = new Vector3();                                       //Vector pos 
+        int numTargets = 0;                                                       //number of targets
+                
+        for (int i = 0; i < m_Targets.Length; i++)                                //loops through max number of players
+        {
+            if (m_Targets[i] == null || !m_Targets[i].isAlive)                    //checks if player is alive
+                continue;
+            
+
+            averagePos += m_Targets[i].gameObject.transform.position;
+            numTargets++;
+        }
+
+        if (numTargets <= 0)
+            followZone = true;
+        else
+            followZone = false;
+
 
         // Add the zone to the avg position if we are going to follow it
         if (followZone)
         {
-            averagePos += (m_Zone.transform.position / 2.0f);
-            numTargets++;
-        }
-
-        for (int i = 0; i < m_Targets.Length; i++)
-        {
-            if (m_Targets[i] == null || !m_Targets[i].isAlive)
-                continue;
-
-            averagePos += m_Targets[i].gameObject.transform.position;
+            //averagePos += (m_Zone.transform.position);
+            averagePos += defaultCamPos.transform.position;
             numTargets++;
         }
 
