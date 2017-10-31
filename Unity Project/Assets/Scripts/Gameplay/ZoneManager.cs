3ba34b51d66c57eaed5e361e02fc723d2b316611
GeneDC <genecollis12@gmail.com>
2017-10-31 12:33:11 +1100

    Changed how the ZoneManager.cs works

--- Unity Project/Assets/Scripts/Gameplay/ZoneManager.cs
+++ Unity Project/Assets/Scripts/Gameplay/ZoneManager.cs
@@ -7,14 +7,29 @@ public class ZoneManager : MonoBehaviour
     [SerializeField]
     int pointsBeforeMoving = 10;
 
+    bool zoneInCentre = true;
+
     [SerializeField]
-    List<Transform> zonePostitions;
+    Transform zoneCentre;
+    [SerializeField]
+    Transform zoneFront;
+    [SerializeField]
+    Transform zoneBack;
 
     int currentZonePosIndex = -1;
 
     GameObject zone;
 
     float pointsLeft = 0;
+
+    public enum ZonePosition
+    {
+        Centre,
+        Front,
+        Back
+    }
+
+    public ZonePosition zonePosition;
 
     void Awake()
     {
@@ -39,21 +54,58 @@ public class ZoneManager : MonoBehaviour
 
     void MoveZone()
     {
-        if (zonePostitions.Count == 0) // If we have no positions don't move
-            return;
-        if (zonePostitions.Count == 1) // If we have 1 position don't move
-            return;
 
-        int newZonePosIndex = Random.Range(0, zonePostitions.Count);
-        // Make sure we chose a new position
-        while(newZonePosIndex == currentZonePosIndex)
+        switch (zonePosition)
         {
-            newZonePosIndex = Random.Range(0, zonePostitions.Count);
-            Debug.Log(newZonePosIndex);
+            case ZonePosition.Centre:
+                if (Random.Range(0, 2) == 0)
+                {
+                    zone.transform.position = zoneFront.position; // Move the zone to front
+                }
+                else
+                {
+                    zone.transform.position = zoneBack.position; // Move the zone to back
+                }
+                break;
+            case ZonePosition.Front:
+                if (Random.Range(0, 2) == 0)
+                {
+                    zone.transform.position = zoneCentre.position; // Move the zone to centre
+                }
+                else
+                {
+                    zone.transform.position = zoneBack.position; // Move the zone to back
+                }
+                break;
+            case ZonePosition.Back:
+                if (Random.Range(0, 2) == 0)
+                {
+                    zone.transform.position = zoneFront.position; // Move the zone to front
+                }
+                else
+                {
+                    zone.transform.position = zoneCentre.position; // Move the zone to centre
+                }
+                break;
+            default:
+                break;
         }
 
-        currentZonePosIndex = newZonePosIndex; // Set the current index
+        //if (zonePostitions.Count == 0) // If we have no positions don't move
+        //    return;
+        //if (zonePostitions.Count == 1) // If we have 1 position don't move
+        //    return;
 
-        zone.transform.position = zonePostitions[currentZonePosIndex].position; // Move the zone to that position
+        //int newZonePosIndex = Random.Range(0, zonePostitions.Count);
+        //// Make sure we chose a new position
+        //while(newZonePosIndex == currentZonePosIndex)
+        //{
+        //    newZonePosIndex = Random.Range(0, zonePostitions.Count);
+        //    Debug.Log(newZonePosIndex);
+        //}
+
+        //currentZonePosIndex = newZonePosIndex; // Set the current index
+
+        //zone.transform.position = zonePostitions[currentZonePosIndex].position; // Move the zone to that position
     }
 }
