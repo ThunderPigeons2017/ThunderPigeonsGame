50e9762c9067123868ca30d49607d275f7ebbc98
kittymori <stirrupprazak.c@gmail.com>
2017-11-14 15:51:21 +1100

    Added a destroyer script for the objects. Added a physic material for the objects

---
+++ Unity Project/Assets/Scripts/Gameplay/ObjectDestroyer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDestroyer : MonoBehaviour {
+	
+	// Destroys Objects tagged 'Destroyable Objects' when it enters the Trigger
+	void OnTriggerEnter (Collider other) {
+
+		if (other.tag == "Destroyable Objects") {
+			Destroy (gameObject);
+			Debug.Log ("Destroyed Object");
+		}
+	}
+}
