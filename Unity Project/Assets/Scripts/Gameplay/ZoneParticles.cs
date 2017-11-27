7da643bc48a7ea6bf3933760cd051faab84c674e
GeneDC <genecollis12@gmail.com>
2017-11-27 12:30:48 +1100

    Implemented new zone particles

--- Unity Project/Assets/Scripts/Gameplay/ZoneParticles.cs
+++ Unity Project/Assets/Scripts/Gameplay/ZoneParticles.cs
@@ -7,8 +7,12 @@ public class ZoneParticles : MonoBehaviour
 
     ZoneControl zoneControl;
 
-    ParticleSystem zoneParticle;
-    Renderer particleRenderer;
+    ParticleSystem primaryParticle;
+    ParticleSystem secondaryParticle;
+    ParticleSystem splashParticle;
+    Renderer primaryRenderer;
+    Renderer secondaryRenderer;
+    Renderer splashRenderer;
 
     [SerializeField]
     Color defaultColour = Color.cyan;
@@ -16,8 +20,14 @@ public class ZoneParticles : MonoBehaviour
     void Awake ()
     {
         zoneControl = transform.parent.GetComponent<ZoneControl>();
-        zoneParticle = GetComponent<ParticleSystem>();
-        particleRenderer = GetComponent<Renderer>();
+        // Find the Particle systems
+        primaryParticle = GetComponent<ParticleSystem>();
+        secondaryParticle = primaryParticle.transform.GetChild(0).GetComponent<ParticleSystem>();
+        splashParticle = secondaryParticle.transform.GetChild(0).GetComponent<ParticleSystem>();
+        // Find the renderers
+        primaryRenderer = GetComponent<Renderer>();
+        secondaryRenderer = primaryParticle.transform.GetChild(0).GetComponent<Renderer>();
+        splashRenderer = secondaryParticle.transform.GetChild(0).GetComponent<Renderer>();
     }
 	
 	void Update ()
@@ -25,26 +35,41 @@ public class ZoneParticles : MonoBehaviour
         // No one is in the zone
         if (zoneControl.playersInZone.Count == 0)
         {
-            zoneParticle.Play();
+            // Play the particles
+            primaryParticle.Play();
+            secondaryParticle.Play();
+            splashParticle.Play();
 
             // Set default colour
-            particleRenderer.material.SetColor("_TintColor", defaultColour);
+            primaryRenderer.sharedMaterial.SetColor("_TintColor", defaultColour);
+            secondaryRenderer.sharedMaterial.SetColor("_TintColor", defaultColour);
+            splashRenderer.sharedMaterial.SetColor("_EmissionColor", defaultColour);
         }
         // One person is in the zone
         else if (zoneControl.playersInZone.Count == 1)
         {
-            zoneParticle.Play();
+
+            // Play the particles
+            primaryParticle.Play();
+            secondaryParticle.Play();
+            splashParticle.Play();
 
             // Set colour to the only player in the zones colour
-            Color playerColor = zoneControl.playersInZone[0].transform.parent.GetComponentInChildren<ColourSetter>().primaryColour;
-            particleRenderer.material.SetColor("_TintColor", playerColor);
+            ColourSetter playerColorSetter = zoneControl.playersInZone[0].transform.parent.GetComponentInChildren<ColourSetter>();
+            primaryRenderer.sharedMaterial.SetColor("_TintColor", playerColorSetter.primaryColour);
+            secondaryRenderer.sharedMaterial.SetColor("_TintColor", playerColorSetter.secondaryColour);
+            splashRenderer.sharedMaterial.SetColor("_EmissionColor", playerColorSetter.primaryColour);
         }
         else
         {
-            // Set to default colour
-            particleRenderer.material.SetColor("_TintColor", defaultColour);
+            // Set default colour
+            primaryRenderer.sharedMaterial.SetColor("_TintColor", defaultColour);
+            secondaryRenderer.sharedMaterial.SetColor("_TintColor", defaultColour);
+            splashRenderer.sharedMaterial.SetColor("_EmissionColor", defaultColour);
             // Pause the animation
-            zoneParticle.Pause();
+            primaryParticle.Pause();
+            secondaryParticle.Pause();
+            splashParticle.Pause();
         }
 
 	}
