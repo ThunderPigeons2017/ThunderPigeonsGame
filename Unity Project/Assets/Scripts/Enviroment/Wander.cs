be6f42d555bac5ccc1e986429a503c1a040d74d8
AnonUser676 <rouie.ortega@live.com>
2017-11-28 15:28:56 +1100

    Comments added on Scripts Wander and Water

--- Unity Project/Assets/Scripts/Enviroment/Wander.cs
+++ Unity Project/Assets/Scripts/Enviroment/Wander.cs
@@ -1,74 +1,82 @@
-﻿using System.Collections;
+﻿/***********************************************
+ * Rummy Robots
+ * by Thunder Pidgeons
+ * 
+ * Menu Camera Control Script
+ ***********************************************/
+//Codes to include basic Unity Functions
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Wander Class
+/// </summary>
 public class Wander : MonoBehaviour
 {
-    Vector3 targetPos;
-
-    [SerializeField]
-    float wanderRadius = 4.0f;
-    //[SerializeField]
-    //float jitterRadius = 1.0f;
-    [SerializeField]
-    float wanderAngle = 35.0f;
-
-    [SerializeField]
-    float speed = 35.0f;
-
-    Vector3 startPos;
-    Quaternion startRot;
-
-    void Start ()
-    {
-        startPos = transform.position;
-        startRot = transform.rotation;
-
-        FindNewTargetPos();
-        MoveToTargetPos();
-    }
-
-    void Update ()
-    {
-        MoveToTargetPos();
-
-        // If we reached the target pos
-        if (Vector3.Distance(targetPos, transform.position) < 1.0f)
-        {
-            // Get a new position
-            FindNewTargetPos();
-        }
-
-        Vector3 vecBetween = (targetPos - transform.position).normalized;
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(vecBetween), Time.deltaTime);
-    }
-
-    void MoveToTargetPos()
-    {
-        float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, targetPos, step);
-    }
-
-    void FindNewTargetPos()
-    {
-        Vector3 newPos = new Vector3();
-
-        Quaternion rot = Quaternion.Euler(Vector3.up * Random.Range(-wanderAngle, wanderAngle));
-        Vector3 newForward = rot * transform.forward;
-
-        // Get the position at the end of the wander radius
-        newPos = newForward * wanderRadius + transform.position;
-
-        // Return our new position
-        targetPos = newPos;
-    }
-
-    public void Respawn()
-    {
-        transform.position = startPos;
-        transform.rotation = startRot;
-
-        FindNewTargetPos();
-        MoveToTargetPos();
-    }
+    Vector3 targetPos;                                                                                                       //
+                                                                                                                             //
+    [SerializeField]                                                                                                         //
+    float wanderRadius = 4.0f;                                                                                               //
+    [SerializeField]                                                                                                         //
+    float wanderAngle = 35.0f;                                                                                               //
+                                                                                                                             //
+    [SerializeField]                                                                                                         //
+    float speed = 35.0f;                                                                                                     //
+                                                                                                                             //
+    Vector3 startPos;                                                                                                        //
+    Quaternion startRot;                                                                                                     //
+                                                                                                                             //
+    void Start ()                                                                                                            //
+    {                                                                                                                        //
+        startPos = transform.position;                                                                                       //
+        startRot = transform.rotation;                                                                                       //
+                                                                                                                             //
+        FindNewTargetPos();                                                                                                  //
+        MoveToTargetPos();                                                                                                   //
+    }                                                                                                                        //
+                                                                                                                             //
+    void Update ()                                                                                                           //
+    {                                                                                                                        //
+        MoveToTargetPos();                                                                                                   //
+                                                                                                                             //
+        // If we reached the target pos                                                                                      //
+        if (Vector3.Distance(targetPos, transform.position) < 1.0f)                                                          //
+        {                                                                                                                    //
+            // Get a new position                                                                                            //
+            FindNewTargetPos();                                                                                              //
+        }                                                                                                                    //
+                                                                                                                             //
+        Vector3 vecBetween = (targetPos - transform.position).normalized;                                                    //
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(vecBetween), Time.deltaTime);      //
+    }                                                                                                                        //
+                                                                                                                             //
+    void MoveToTargetPos()                                                                                                   //
+    {                                                                                                                        //
+        float step = speed * Time.deltaTime;                                                                                 //
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, step);                                       //
+    }                                                                                                                        //
+                                                                                                                             //
+    void FindNewTargetPos()                                                                                                  //
+    {                                                                                                                        //
+        Vector3 newPos = new Vector3();                                                                                      //
+                                                                                                                             //
+        Quaternion rot = Quaternion.Euler(Vector3.up * Random.Range(-wanderAngle, wanderAngle));                             //
+        Vector3 newForward = rot * transform.forward;                                                                        //
+                                                                                                                             //
+        // Get the position at the end of the wander radius                                                                  //
+        newPos = newForward * wanderRadius + transform.position;                                                             //
+                                                                                                                             //
+        // Return our new position                                                                                           //
+        targetPos = newPos;                                                                                                  //
+    }                                                                                                                        //
+                                                                                                                             //
+    public void Respawn()                                                                                                    //
+    {                                                                                                                        //
+        transform.position = startPos;                                                                                       //
+        transform.rotation = startRot;                                                                                       //
+                                                                                                                             //
+        FindNewTargetPos();                                                                                                  //
+        MoveToTargetPos();                                                                                                   //
+    }                                                                                                                        //
 }
--- Unity Project/Assets/Scripts/Enviroment/Water.cs
+++ Unity Project/Assets/Scripts/Enviroment/Water.cs
@@ -1,96 +1,118 @@
-﻿using UnityEngine;
+﻿/***********************************************
+ * Rummy Robots
+ * by Thunder Pidgeons
+ * 
+ * Water Environment Script
+ ***********************************************/
+//Codes to include basic Unity Functions
+
+using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Water Class Begins
+/// </summary>
+public class Water : MonoBehaviour                                                                                                      
+{                                                                                                                                       
+    [SerializeField]                                                                                                                    //public variables that can be set from Inspector for producers 
+    [Tooltip("The speed that the waves move")]                                                                                          //Tooltip for waveSpeed
+    float waveSpeed = 0.5f;                                                                                                             //waveSpeed variable
+    [SerializeField]                                                                                                                    
+    [Tooltip("The +/- y height of the main waves")]                                                                                     //Tooltip for waveHeight
+    float waveHeight = 0.48f;                                                                                                           //Varaible for waveHeight
+    [SerializeField]                                                                                                                    
+    [Tooltip("The +/- y height of the triangulation (kinda like choppyness at sea)")]                                                   //Tooltip for triangulationHeight
+    float triangulationHeight = 0.1f;                                                                                                   //variable for triangulationHeight
+                                                                                                                                        
+    Mesh mesh;                                                                                                                          //Mesh Varaible called mesh
+    Vector3[] verts;                                                                                                                    //Mesh Vector varaible called verts
+                                                                                                                                        
+    float offsetX;                                                                                                                      //offsetX
+    float offsetY;                                                                                                                      //offsetY
+    
+    /// <summary>
+    /// On Boot of Game
+    /// </summary>
+    void Awake()                                                                                                                        
+    {                                                                                                                                   
+        MeshFilter mf = GetComponent<MeshFilter>();                                                                                     //variable mf gets data from MeshFilter
+        MakeMeshLowPoly(mf);                                                                                                            //calls function MakemeshLowPoly passing in mf
+    }                                                                                                                                   
+    
+    /// <summary>
+    /// On Start of Program
+    /// </summary>
+    void Start()                                                                                                                        
+    {                                                                                                                                   
+        string seed = Time.time.ToString();                                                                                             //Time is set as variable seed
+                                                                                                                                        
+        offsetX = seed.GetHashCode() % 10000;                                                                                           //offsetX is set as seed percentage
+        offsetY = offsetX;                                                                                                              //offsetY set as offsetX
+    }                                                                                                                                   
 
+    /// <summary>
+    /// Makes Mesh passed in parameter as low poly
+    /// </summary>
+    /// <param name="mf">Meshfilter passed in as parameter</param>
+    /// <returns></returns>
+    MeshFilter MakeMeshLowPoly(MeshFilter mf)                                                                                           
+    {                                                                                                                                   
+                                                                                                                                        
+        mesh = mf.sharedMesh;                                                                                                           //We want each tri to have its own unshared verts
+        Vector3[] oldVerts = mesh.vertices;                                                                                             //triangle Vectors from mesh is stored in oldVerts
+        int[] triangles = mesh.triangles;                                                                                               //mesh.triangle values is stored in triangles int variable
+        Vector3[] vertices = new Vector3[triangles.Length];                                                                             //triangles length vector is placed in vertices array
 
-public class Water : MonoBehaviour
-{
-    [SerializeField]
-    [Tooltip("The speed that the waves move")]
-    float waveSpeed = 0.5f;
-    [SerializeField]
-    [Tooltip("The +/- y height of the main waves")]
-    float waveHeight = 0.48f;
-    [SerializeField]
-    [Tooltip("The +/- y height of the triangulation (kinda like choppyness at sea)")]
-    float triangulationHeight = 0.1f;
+        for (int i = 0; i < triangles.Length; i++)                                                                                      //loops through all triangles
+        {                                                                                                                               
+            vertices[i] = oldVerts[triangles[i]];                                                                                       //sets vertices as oldVerts Triangles
+            triangles[i] = i;                                                                                                           //triangles is set as counter
+        }                                                                                                                               
 
-    Mesh mesh;
-    Vector3[] verts;
+        mesh.vertices = vertices;                                                                                                       //mesh vertices is set as new vertices
+        mesh.triangles = triangles;                                                                                                     //mesh triangles is set as new triangles
+        mesh.RecalculateBounds();                                                                                                       //mesh Recalculate Bounds
+        mesh.RecalculateNormals();                                                                                                      //mesh recalculates normals
+        verts = mesh.vertices;                                                                                                          //verts is set as new mesh vertices
+        return mf;                                                                                                                      //returns the calculated mesh
+    }                                                                                                                                   
 
-    float offsetX;
-    float offsetY;
+    /// <summary>
+    /// Every time Game updates
+    /// </summary>
+    void Update()                                                                                                                       
+    {                                                                                                                                   
+        CalcWave();                                                                                                                     //Calls CalcWave function
+        offsetX += Time.deltaTime * waveSpeed;                                                                                          //offsetX is computed according to deltatime and wavespeed
+    }                                                                                                                                   
 
-    void Awake()
-    {
-        MeshFilter mf = GetComponent<MeshFilter>();
-        MakeMeshLowPoly(mf);
-    }
-
-    void Start()
-    {
-        string seed = Time.time.ToString();
-
-        offsetX = seed.GetHashCode() % 10000;
-        offsetY = offsetX;
-    }
-
-    MeshFilter MakeMeshLowPoly(MeshFilter mf)
-    {
-        // We want each tri to have its own unshared verts
-
-        mesh = mf.sharedMesh;
-        Vector3[] oldVerts = mesh.vertices;
-        int[] triangles = mesh.triangles;
-        Vector3[] vertices = new Vector3[triangles.Length];
-        for (int i = 0; i < triangles.Length; i++)
-        {
-            vertices[i] = oldVerts[triangles[i]];
-            triangles[i] = i;
-        }
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
-        mesh.RecalculateBounds();
-        mesh.RecalculateNormals();
-        verts = mesh.vertices;
-        return mf;
-    }
-
-    void Update()
-    {
-        CalcWave();
-        offsetX += Time.deltaTime * waveSpeed;
-    }
-
-    void CalcWave()
-    {
-        // Loop through every vert and change its y
-        for (int i = 0; i < verts.Length; i++)
-        {
-            Vector3 v = verts[i];
-            v.y = 0; // Reset the y so we can add the new values
-
-            // Add position to make it relatice to world space
-            Vector3 worldPos = v + transform.position;
-
-            // Perlin noise wave 1 (bigger underlying wave)
-            float xCoord = offsetX + worldPos.x / 100;
-            float zCoord = offsetY + worldPos.z / 100;
-            v.y += Helper.Remap(Mathf.PerlinNoise(xCoord, zCoord), 0, 1, 0, waveHeight) - waveHeight / 2;
-
-            // Perlin noise wave 2 (small more frequent waves to add choppyness to the top)
-            xCoord = offsetX + worldPos.x / 2;
-            zCoord = offsetY + worldPos.z / 2;
-            v.y += Helper.Remap(Mathf.PerlinNoise(xCoord, zCoord), 0, 1, 0, triangulationHeight) - triangulationHeight / 2;
-
-            //v.y += waveHeight3 / 5 * Mathf.Sin(Time.time * Mathf.PI * 2.0f * 0.1f);
-
-            verts[i] = v;
-        }
-        mesh.vertices = verts;
-        mesh.RecalculateNormals();
-        mesh.MarkDynamic();
-
-        GetComponent<MeshFilter>().mesh = mesh;
-    }
-}
+    /// <summary>
+    /// Calcwave function that calculates the wave
+    /// </summary>
+    void CalcWave()                                                                                                                     
+    {                                                                                                                                   
+                                                                                                                                        
+        for (int i = 0; i < verts.Length; i++)                                                                                          // Loop through every vert and change its y
+        {                                                                                                                               
+            Vector3 v = verts[i];                                                                                                       
+            v.y = 0;                                                                                                                    // Reset the y so we can add the new values
+                                                                                                                                        
+            Vector3 worldPos = v + transform.position;                                                                                  // Add position to make it relatice to world space
+            
+            float xCoord = offsetX + worldPos.x / 100;                                                                                  // Perlin noise wave 1 (bigger underlying wave)
+            float zCoord = offsetY + worldPos.z / 100;                                                                                  //
+            v.y += Helper.Remap(Mathf.PerlinNoise(xCoord, zCoord), 0, 1, 0, waveHeight) - waveHeight / 2;                               
+                                                         
+            xCoord = offsetX + worldPos.x / 2;                                                                                          // Perlin noise wave 2 (small more frequent waves to add choppyness to the top)
+            zCoord = offsetY + worldPos.z / 2;                                                                                          
+            v.y += Helper.Remap(Mathf.PerlinNoise(xCoord, zCoord), 0, 1, 0, triangulationHeight) - triangulationHeight / 2;             
+                                                                                                                                        
+            verts[i] = v;                                                                                                               
+        }                                                                                                                               
+        mesh.vertices = verts;                                                                                                          
+        mesh.RecalculateNormals();                                                                                                      
+        mesh.MarkDynamic();                                                                                                             
+                                                                                                                                        
+        GetComponent<MeshFilter>().mesh = mesh;                                                                                         
+    }                                                                                                                                   
+}                                                                                                                                       
